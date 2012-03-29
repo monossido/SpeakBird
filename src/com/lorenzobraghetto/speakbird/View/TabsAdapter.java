89f201809f9189478a974d2a7869761f6111824a
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2012-03-29 12:47:58 +0200

    Copyright and PreferenceActivity-Layout minor fix

--- src/com/lorenzobraghetto/speakbird/View/TabsAdapter.java
+++ src/com/lorenzobraghetto/speakbird/View/TabsAdapter.java
@@ -1,3 +1,21 @@
+/*  Copyright 2012
+ *	Lorenzo Braghetto monossido@lorenzobraghetto.com
+ *      This file is part of SpeakBird <https://github.com/monossido/SpeakBird>
+ *      
+ *      SpeakBird is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      SpeakBird is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with SpeakBird  If not, see <http://www.gnu.org/licenses/>.
+ *      
+ */
 package com.lorenzobraghetto.speakbird.View;
 
 import java.util.ArrayList;
